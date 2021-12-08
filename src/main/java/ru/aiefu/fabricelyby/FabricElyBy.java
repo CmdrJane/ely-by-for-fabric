0aae8a5c2b6a6c3ddb08ddd0011d40123e41e9cd
CmdrJane <commanderjanedoe@gmail.com>
2021-12-08 19:33:46 +0300

    -

--- src/main/java/ru/aiefu/fabricelyby/FabricElyBy.java
+++ src/main/java/ru/aiefu/fabricelyby/FabricElyBy.java
@@ -16,6 +16,8 @@ public class FabricElyBy implements DedicatedServerModInitializer {
 
 	public static final Logger LOGGER = LogManager.getLogger("fabricelyby");
 
+	public static String serverToken;
+
 	@Override
 	public void onInitializeServer() {
 		LOGGER.info("Ely-by skin system loaded!");
@@ -25,9 +27,12 @@ public class FabricElyBy implements DedicatedServerModInitializer {
 	public static JsonElement getSkinData(String playerName) {
 		HttpURLConnection connection;
 		try {
-			connection = (HttpURLConnection) new URL("http://skinsystem.ely.by/textures/signed/" + playerName).openConnection();
+			connection = (HttpURLConnection) new URL(String.format("http://skinsystem.ely.by/textures/signed/%s?token=%s", playerName, serverToken)).openConnection();
+			if(connection.getResponseCode() == 204){
+				FabricElyBy.LOGGER.warn(String.format("Unable to retrieve skin textures for %s! Textures not found on server!", playerName));
+				return null;
+			}
 			return JsonParser.parseReader(new InputStreamReader(connection.getInputStream()));
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
